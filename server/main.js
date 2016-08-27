80d5a3e608550d151211e5ce6f8033e6169a1ce9
Andrey Melnik <andrey.melnik@icloud.com>
2016-08-27 08:41:42 +0300

    Add new entry and remove functions

--- server/main.js
+++ server/main.js
@@ -3,4 +3,12 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
   Tasks = new Mongo.Collection('tasks');
+  
+  Meteor.methods({
+    clear: function() {
+      Tasks.remove({})
+    }
+  });
 });
+
+
